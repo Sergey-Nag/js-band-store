b6bc2bdbb4973b4ba189a85e40a3efb45db9e4c1
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-03-28 04:20:31 +0300

    test for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
+import { AUTH_USER } from './store/types/userTypes';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+afterEach(cleanup);
+
+const renderWithRedux = (component) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+});
+
+describe('"App" component test', () => {
+  test('"App" render "login" component if token is null', () => {
+    const app = renderWithRedux(<App />);
+    const reduxStore = store.getState();
+
+    expect(reduxStore.user.token).toBe(null);
+    expect(app.getAllByTestId('Login').length).toBe(1);
+  });
+
+  test('"App" render "Catalog" component if token is\'nt null', () => {
+    store.dispatch({
+      type: AUTH_USER,
+      payload: {
+        username: 'Test',
+        token: 'test',
+        avatar: 'test',
+      },
+    });
+    const reduxStore = store.getState();
+    console.log(reduxStore);
+    expect(reduxStore.user.token).not.toBe(null);
+  });
+});
